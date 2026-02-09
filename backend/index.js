e7c54a08a93557817854eada7d74b61b2f9f41ec
Ubaldo Narvaez <satou.matsuzaka.3443@gmail.com>
2026-02-09 17:58:31 -0600

    Se creo el servidor con express

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,17 @@
-console.log("IJole");
+// Iportaciones:
+const express = require('express');
+
+// Instancia del servidor:
+const app = express();
+
+// Routing:
+app.get('/', (req, res) => {
+    res.send("Hola mundo en express");
+});
+
+// Generar servidor:
+app.listen(4000, () => {
+    console.log("Servidor funcionando.");
+});
+
+
